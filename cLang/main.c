62eace824eed01839b72f06ecebb7e3378a3d978
abc <polankivarun1729@gmail.com>
2018-11-06 17:53:46 +0530

    Initial Commit

---
+++ cLang/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cLang
+//
+//  Created by Varun Kumar on 06/11/18.
+//  Copyright © 2018 Varun Kumar. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
